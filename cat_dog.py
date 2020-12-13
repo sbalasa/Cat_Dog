7ec3c359a2e5e025ff9619e080f694df2852c5cf
Santhosh Balasa <santhosh.kbr@gmail.com>
2020-12-13 19:20:49 +0800

    Create cat_dog.py

---
+++ cat_dog.py
@@ -0,0 +1,29 @@
+class Cat:
+    def __init__(self):
+        self.sound = "Meow Meow"
+
+    def talk(self):
+        return self.sound
+
+
+class Dog:
+    def __init__(self):
+        self.sound = "Bow Bow"
+
+    def talk(self):
+        return self.sound
+
+
+def main():
+    c = Cat()
+    d = Dog()
+
+    print("Cat says: ", c.talk())
+    print("Dog says: ", d.talk())
+    print()
+    print("Cat says: ", c.__class__.talk(Dog))
+    print("Dog says: ", d.__class__.talk(Cat))
+
+
+if __name__ == "__main__":
+    main()
